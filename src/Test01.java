a081fe2c4c1e994575a522583c99bf714ad21e2f
lixiang <1391598479@qq.com>
2022-07-13 10:16:36 +0800

    second commit

--- src/Test01.java
+++ src/Test01.java
@@ -4,37 +4,38 @@ import java.util.List;
 public class Test01 {
 
     public static void main(String[] args) {
-        List<SkyWechatMenu> data = new ArrayList<>();
-        SkyWechatMenu s1 = new SkyWechatMenu();
-        s1.setId("1");
-        s1.setParentId(null);
-
-        SkyWechatMenu s2 = new SkyWechatMenu();
-        s2.setId("2");
-        s2.setParentId(null);
-
-        SkyWechatMenu s3 = new SkyWechatMenu();
-        s3.setId("3");
-        s3.setParentId("2");
-
-        SkyWechatMenu s4 = new SkyWechatMenu();
-        s4.setId("4");
-        s4.setParentId("2");
-
-        SkyWechatMenu s5 = new SkyWechatMenu();
-        s5.setId("5");
-        s5.setParentId("2");
-
-        data.add(s1);
-        data.add(s2);
-        data.add(s3);
-        data.add(s4);
-        data.add(s5);
-        SkyWechatMenu s6 = new SkyWechatMenu();
-        s6.setId("5");
-        s6.setParentId("2");
-
-        System.out.println(deepbutton(data,null));
+//        List<SkyWechatMenu> data = new ArrayList<>();
+//        SkyWechatMenu s1 = new SkyWechatMenu();
+//        s1.setId("1");
+//        s1.setParentId(null);
+//
+//        SkyWechatMenu s2 = new SkyWechatMenu();
+//        s2.setId("2");
+//        s2.setParentId(null);
+//
+//        SkyWechatMenu s3 = new SkyWechatMenu();
+//        s3.setId("3");
+//        s3.setParentId("2");
+//
+//        SkyWechatMenu s4 = new SkyWechatMenu();
+//        s4.setId("4");
+//        s4.setParentId("2");
+//
+//        SkyWechatMenu s5 = new SkyWechatMenu();
+//        s5.setId("5");
+//        s5.setParentId("2");
+//
+//        data.add(s1);
+//        data.add(s2);
+//        data.add(s3);
+//        data.add(s4);
+//        data.add(s5);
+//        SkyWechatMenu s6 = new SkyWechatMenu();
+//        s6.setId("5");
+//        s6.setParentId("2");
+//
+//        System.out.println(deepbutton(data,null));
+        System.out.println("hello world");
     }
 
     //
